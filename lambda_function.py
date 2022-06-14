a8492e5a3ddd3b8054e9fd137692487fde7cbfec
Edinho <git@final04.com>
2022-06-14 16:19:09 -0300

    Add adapter to work with lambda functions

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+import logging.config
+
+from mangum import Mangum
+
+from service import main
+
+logging.config.fileConfig(
+    "logging.conf",
+    disable_existing_loggers=False,
+)
+logger = logging.getLogger(__name__)
+
+
+def lambda_handler(event, context):
+    """Call relevant code and return status_code."""
+
+    asgi_handler = Mangum(main.app)
+    response = asgi_handler(event, context)
+    return response
